bca99df8b91fba12f7f8dbe25e89968904a2dfd1
Zeynep <zeynepkaya_26@hotmail.com>
2022-01-13 23:23:47 +0000

    Creating the full functioning login page with an logout route

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,11 @@ const router = express.Router();
 
 module.exports = (db) => {
 
-    router.get('/', (req, res) => {
-        res.render("login-form");
-     });
-
     router.post("/", (req, res) => {
         let email = req.body.email;
         let password = req.body.password;
         return db.query(`
-      SELECT id, email, password
+      SELECT id, name, email, password 
       FROM users
       WHERE email = $1
     `, [email])
@@ -22,10 +18,13 @@ module.exports = (db) => {
                     console.log("abc");
                     if (bcrypt.compareSync(password, response.rows[0].password)) {
                         console.log("def");
-                        let userName = response.rows[0].name;
-                        let userID = response.rows[0].id;
-                        req.session["userName"] = userName;
-                        req.session["userID"] = userID;
+                        let user_name = response.rows[0].name;
+                        let user_id = response.rows[0].id;
+                        let user_email = response.rows[0].email;
+                        req.session["user_name"] = user_name;
+                        req.session["user_id"] = user_id;
+                        req.session["user_email"] = user_email;
+                        console.log(req.session)
                         return res.redirect("/");
                     } else {
                       return res.redirect("/login");
